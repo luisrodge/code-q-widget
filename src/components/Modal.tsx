a5fe64dfa23c69bac3f06174c14efbeea689af28
Luis Rodriguez <luisrodge@outlook.com>
2022-01-16 11:27:37 -0600

    Add dont show again btn

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,7 +4,8 @@ import Cookies from "js-cookie";
 
 import "../styles/modal.css";
 
-const LS_KEY = "cqwd";
+const COOKIE_DAY_KEY = "qw_d";
+const COOKIE_SHOW_KEY = "s_qw";
 
 interface Props {
   color?: string;
@@ -16,31 +17,48 @@ export default function Modal({ children, footer }: Props): VNode {
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
+    if (Cookies.get(COOKIE_SHOW_KEY)) return;
+
     const day = new Date().getDay().toString();
 
-    const cookieDay = Cookies.get(LS_KEY);
+    const cookieDay = Cookies.get(COOKIE_DAY_KEY);
 
     if (cookieDay) {
       if (day !== cookieDay) {
         setIsOpen(true);
-        Cookies.set(LS_KEY, day, { expires: 3 });
+        Cookies.set(COOKIE_DAY_KEY, day, { expires: 3 });
       }
     } else {
       setIsOpen(true);
-      Cookies.set(LS_KEY, day, { expires: 3 });
+      Cookies.set(COOKIE_DAY_KEY, day, { expires: 3 });
     }
   }, [setIsOpen]);
 
   if (!isOpen) return null;
 
-  const close = () => setIsOpen(false);
+  const onClose = () => setIsOpen(false);
+
+  const onDontShowAgain = () => {
+    Cookies.set(COOKIE_SHOW_KEY, "false", { expires: 31 });
+    setIsOpen(false);
+  };
 
   return (
     <div className="modal-window">
       <div>
-        <button className="modal-close" onClick={close}>
-          Close
-        </button>
+        <div className="modal-header">
+          <p class="question-text">Code question of the day</p>
+
+          <div>
+            <button className="modal-btn" onClick={onDontShowAgain}>
+              Don't show again
+            </button>
+            <button className="modal-btn" onClick={onClose}>
+              Close
+            </button>
+          </div>
+        </div>
+
         <div className="modal-body">{children}</div>
         {footer && footer.length > 0 && (
           <div className="modal-footer">{footer.map((node) => node)}</div>
